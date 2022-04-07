e4d842893acd52a541a9fb51dd0321c89f70b1c9
My Name <ben.d.maryland@gmail.com>
2022-04-06 23:23:32 -0400

    replace a char in a string

---
+++ JS/map/replaceLettinString.js
@@ -0,0 +1,10 @@
+
+function replacer(string, oldLet, newLet ){
+let splitString = string.split("")
+
+
+ splitString = splitString.map(letter=>(letter ===oldLet? letter= newLet : letter ))
+console.log(splitString.join(""))
+}
+
+replacer("hello", "h", "y")
---
+++ JS/map/test.js
@@ -0,0 +1,10 @@
+
+let numArray = [1,2,3,4,5]
+let nameArray = ["Ben", "Tom", "Dave","Benjamin"]
+
+
+
+let result = nameArray.map((name,i)=>name+ i )
+console.log(result)
+
+
