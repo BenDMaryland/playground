df3b0c8e568c928726cd735898df514294ea1004
My Name <ben.d.maryland@gmail.com>
2022-04-08 22:21:27 -0400

    Almost there

--- JS/challenges/Anagram.js
+++ JS/challenges/Anagram.js
@@ -1,4 +1,45 @@
-truExamp= anagram('finder', 'Friend')  
- falseExamp =anagram('hello', 'bye') 
+truExamp=['finder', 'Friend']
+ falseExamp =['hello', 'bye']
 
- 
+ function anagramFinder( examp){
+let wordOne = examp[0].toLowerCase().split('')
+let wordTwo = examp[1].toLowerCase().split('')
+let key=[]
+let keyOne=[]
+let keyTwo=[]
+
+function buildKey(letter,operator){
+let obj = {}
+key.find(digit=>(Object.keys(digit)==letter)) == undefined? (obj[letter]=0 , key = [...key,obj]): null
+operator === "plus"?
+key.forEach(digit=> (Object.keys(digit)==letter?digit[letter]++:  null ))
+:
+key.forEach(digit=> (Object.keys(digit)==letter?digit[letter]--:  null ))
+}
+
+
+
+
+
+
+wordOne.map(letter=>buildKey(letter,"plus"))
+// keyOne= key
+// key=[]
+wordTwo.map(letter=>buildKey(letter,"minus"))
+//keyTwo= key
+
+
+
+console.log(key)
+console.log(Object.values(key).reduce(key=> key))
+// console.log("_____________")
+// console.log( "Key one is ",keyOne)
+// console.log("_____________")
+// console.log( "Key two is ",key)
+// console.log("_____________")
+
+
+ }
+
+
+ anagramFinder(truExamp)
