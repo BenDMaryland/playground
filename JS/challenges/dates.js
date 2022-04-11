eaf0871242353c74007285e1390938812f4ee7c6
My Name <ben.d.maryland@gmail.com>
2022-04-11 10:00:51 -0400

    friday the thirteenth

--- JS/challenges/dates.js
+++ JS/challenges/dates.js
@@ -1,4 +1,18 @@
 
+function fridayTheThirteenth(year){
+let d = new Date("July 23, 1983 ");
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+let day = d.getDay()
+
+for(let i =1; i !=13; i++){
+d = new Date(`${i} 13, ${year} `);
+ d.getDay() ===5 ? console.log(`The month ${monthNames[i-1]}, has a friday the thirteenth in  ${year}`) : null
+
+}
+}
 
 
 
@@ -8,9 +22,8 @@
 
 
 
+fridayTheThirteenth(2022)
 
-
-
-const d = new Date("July 23, 1983 ");
-let day = d.getDay("July 22, 1983 ")
-console.log(day)
+// const d = new Date("2 23, 1983 ");
+// let day = d.getDay()
+// console.log(day)
